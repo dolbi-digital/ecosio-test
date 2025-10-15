1e01b7d8788c5ad0289d4075d2119ef75d94d006
dolbi_digital <dolbilov@gmail.com>
2025-10-15 14:36:00 +0200

    added api tests

--- src/test/java/api/ApiTests.java
+++ src/test/java/api/ApiTests.java
@@ -1,40 +1,64 @@
 package api;
 
-import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.*;
 
 public class ApiTests extends BaseApiTest {
 
     @Test()
-    public void people_list_should_not_be_empty() {
-        given()
-                .when()
-                .get("/people/")
-                .then()
-                .statusCode(200)
-                .contentType(ContentType.JSON)
+    public void people_list_fields() {
+        Response resp = GET("/people/");
+        resp.then()
+                .spec(okJson)
+                .body(matchesJsonSchemaInClasspath("schemas/people-list.schema.json"))
                 .body("count", greaterThan(0))
-                .body("results.size()", greaterThan(0))
-                .body("results[0].name", not(isEmptyOrNullString()))
-                .body("results[0].height", not(isEmptyOrNullString()))
-                .body("results[0]", hasKey("films"));
+                .body("next", anyOf(matchesPattern("^https://swapi\\.dev/api/people/\\?page=\\d+$"), nullValue()))
+                .body("previous", anyOf(matchesPattern("^https://swapi\\.dev/api/people/\\?page=\\d+$"), nullValue()))
+                .body("results.size()", greaterThan(0));
     }
 
-    @Test(description = "GET /people/1/ returns Luke Skywalker with films")
-    public void person_one_should_be_luke() {
-        given()
-                .when()
-                .get("/people/1/")
-                .then()
-                .statusCode(200)
-                .contentType(ContentType.JSON)
-                .body("name", equalTo("Luke Skywalker"))
-                .body("films", not(empty()))
-                .body("mass", not(isEmptyOrNullString()))
-                .body("birth_year", matchesPattern("\\d{2}BBY|\\d{2}ABY|unknown"));
+    @Test(description = "NEGATIVE: /people/55555/ → 404 Not Found")
+    public void person_not_found_404() {
+        Response resp = GET("/people/55555/");
+        resp.then()
+                .spec(notFoundJson)
+                .body("detail", equalTo("Not found"));
+    }
+
+    @DataProvider(name = "personIds")
+    public Object[][] personIds() {
+        return new Object[][]{
+                {1, "Luke Skywalker"},
+                {2, "C-3PO"},
+                {3, "R2-D2"},
+                {4, "Darth Vader"},
+                {5, "Leia Organa"}
+        };
+    }
+
+    @Test(dataProvider = "personIds")
+    public void person_schema_and_core_fields(int personId, String expectedName) {
+        Response resp = GET("/people/{id}/", personId);
+        resp.then()
+                .spec(okJson)
+                .body(matchesJsonSchemaInClasspath("schemas/person.schema.json"))
+                .body("name", equalTo(expectedName))
+                .body("$", allOf(
+                        hasKey("height"), hasKey("mass"),
+                        hasKey("hair_color"), hasKey("skin_color"), hasKey("eye_color"),
+                        hasKey("birth_year"), hasKey("gender"),
+                        hasKey("homeworld"), hasKey("films"), hasKey("species"),
+                        hasKey("vehicles"), hasKey("starships"),
+                        hasKey("created"), hasKey("edited"), hasKey("url")
+                ))
+                .body("url", matchesPattern("^https://swapi\\.dev/api/people/" + personId + "/?$"))
+                .body("homeworld", notNullValue())
+                .body("films", notNullValue())
+                .body("species", notNullValue())
+                .body("vehicles", notNullValue())
+                .body("starships", notNullValue());
     }
 }
-
--- src/test/java/api/BaseApiTest.java
+++ src/test/java/api/BaseApiTest.java
@@ -2,15 +2,46 @@ package api;
 
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
 import org.testng.annotations.BeforeClass;
 import utils.Config;
 
+import static io.restassured.RestAssured.given;
+
 public class BaseApiTest {
+
+    protected RequestSpecification req;
+    protected ResponseSpecification okJson;
+    protected ResponseSpecification notFoundJson;
 
     @BeforeClass(alwaysRun = true)
     public void apiSetup() {
         RestAssured.baseURI = Config.get("baseApiUrl");
-        RestAssured.filters(new AllureRestAssured());
+
+        req = new RequestSpecBuilder()
+                .setBaseUri(RestAssured.baseURI)
+                .setAccept(ContentType.JSON)
+                .setContentType(ContentType.JSON)
+                .addFilter(new AllureRestAssured())
+                .build();
+
+        okJson = new ResponseSpecBuilder()
+                .expectStatusCode(200)
+                .expectContentType(ContentType.JSON)
+                .build();
+
+        notFoundJson = new ResponseSpecBuilder()
+                .expectStatusCode(404)
+                .expectContentType(ContentType.JSON)
+                .build();
+    }
+
+    protected Response GET(String path, Object... uriParams) {
+        return given().spec(req).when().get(path, uriParams);
     }
 }
-
